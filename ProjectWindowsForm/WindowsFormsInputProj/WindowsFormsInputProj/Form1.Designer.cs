4af21df203db77e710e59f5bfda8fba653d28bc8
Avel Moscare <avelqmoscare@gmail.com>
2018-08-27 00:58:35 +0800

    Copro 3
    Copro 3 - project

---
+++ ProjectWindowsForm/WindowsFormsInputProj/WindowsFormsInputProj/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsInputProj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCon = new System.Windows.Forms.Button();
+            this.btnDis = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtbox = new System.Windows.Forms.TextBox();
+            this.btnGr = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnBlack = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.btnGr)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnBlack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCon
+            // 
+            this.btnCon.Location = new System.Drawing.Point(12, 226);
+            this.btnCon.Name = "btnCon";
+            this.btnCon.Size = new System.Drawing.Size(75, 23);
+            this.btnCon.TabIndex = 0;
+            this.btnCon.Text = "Connect";
+            this.btnCon.UseVisualStyleBackColor = true;
+            this.btnCon.Click += new System.EventHandler(this.btnCon_Click);
+            // 
+            // btnDis
+            // 
+            this.btnDis.Enabled = false;
+            this.btnDis.Location = new System.Drawing.Point(158, 226);
+            this.btnDis.Name = "btnDis";
+            this.btnDis.Size = new System.Drawing.Size(75, 23);
+            this.btnDis.TabIndex = 1;
+            this.btnDis.Text = "Disconnect";
+            this.btnDis.UseVisualStyleBackColor = true;
+            this.btnDis.Click += new System.EventHandler(this.btnDis_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(311, 226);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(374, 146);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // txtbox
+            // 
+            this.txtbox.Enabled = false;
+            this.txtbox.Location = new System.Drawing.Point(12, 186);
+            this.txtbox.Name = "txtbox";
+            this.txtbox.Size = new System.Drawing.Size(260, 20);
+            this.txtbox.TabIndex = 4;
+            this.txtbox.TextChanged += new System.EventHandler(this.txtbox_TextChanged);
+            // 
+            // btnGr
+            // 
+            this.btnGr.Image = global::WindowsFormsInputProj.Properties.Resources.green_arrow_hi;
+            this.btnGr.Location = new System.Drawing.Point(278, 186);
+            this.btnGr.Name = "btnGr";
+            this.btnGr.Size = new System.Drawing.Size(47, 23);
+            this.btnGr.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnGr.TabIndex = 7;
+            this.btnGr.TabStop = false;
+            this.btnGr.Visible = false;
+            this.btnGr.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsInputProj.Properties.Resources.oui_icons_42_128;
+            this.pictureBox2.Location = new System.Drawing.Point(331, 186);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(55, 20);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // btnBlack
+            // 
+            this.btnBlack.Image = ((System.Drawing.Image)(resources.GetObject("btnBlack.Image")));
+            this.btnBlack.Location = new System.Drawing.Point(278, 186);
+            this.btnBlack.Name = "btnBlack";
+            this.btnBlack.Size = new System.Drawing.Size(47, 20);
+            this.btnBlack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnBlack.TabIndex = 5;
+            this.btnBlack.TabStop = false;
+            this.btnBlack.Click += new System.EventHandler(this.btnBlack_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 261);
+            this.Controls.Add(this.btnGr);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnBlack);
+            this.Controls.Add(this.txtbox);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDis);
+            this.Controls.Add(this.btnCon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmClose);
+            ((System.ComponentModel.ISupportInitialize)(this.btnGr)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnBlack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCon;
+        private System.Windows.Forms.Button btnDis;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtbox;
+        private System.Windows.Forms.PictureBox btnBlack;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox btnGr;
+    }
+}
+
---
+++ ProjectWindowsForm/WindowsFormsInputProj/WindowsFormsInputProj/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsInputProj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCon_Click(object sender, EventArgs e)
+        {
+            btnCon.Enabled = false;
+            btnDis.Enabled = true;
+            txtbox.Enabled = true;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            DateTime date = DateTime.Now;
+            String input = txtbox.Text;
+            string comp = "";
+            switch (input)
+            {
+                case "Hello":
+                    comp = "Hi";
+                    break;
+                case "Hi":
+                    comp = "Hello";
+                    break;
+                case "How are you?":
+                    comp = "I'm fine";
+                    break;
+                case "What is date Now":
+                    comp = date.ToLongDateString();
+                    break;
+                default:
+                    comp = "The text is not recognize";
+                    break;
+            }
+            richTextBox1.AppendText(date.ToString() + "\nYou: " + input + "\nComputer: " + comp + "\n");
+            txtbox.Text = "";
+        }
+
+        private void btnBlack_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You don't have an any text please put text in textbox");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtbox_TextChanged(object sender, EventArgs e)
+        {
+            if (txtbox.Text == "")
+            {
+                btnBlack.Visible = true;
+                btnGr.Visible = false;
+            }
+            else
+            {
+                btnGr.Visible = true;
+                btnBlack.Visible = false;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+           
+            if (txtbox.Text == "")
+            {
+                MessageBox.Show("We don't have a text to clear");
+            }
+            else
+            {
+                txtbox.Text = "";
+            }
+        }
+
+        private void btnDis_Click(object sender, EventArgs e)
+        {
+            btnDis.Enabled = false;
+            btnCon.Enabled = true;
+            richTextBox1.Clear();
+            txtbox.Enabled = false;
+            txtbox.Text = "";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Do you really want to exit?", "Close Form", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes){
+                this.Close();
+                }
+        }
+
+        private void frmClose(object sender, FormClosingEventArgs e)
+        {
+           
+            }
+        }
+    }
+
